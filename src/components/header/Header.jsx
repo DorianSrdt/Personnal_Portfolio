ea4e08d4f34c9beaf5b92e75e6c9ea1ff9db8a74
DorianSrdt <dorian.sacerdote@gmail.com>
2022-05-15 23:08:41 +0200

    header down & responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+import ME from '../../assets/me-remove.png'
 
-function header() {
+function Header() {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Dorian Sacerdote</h1>
+        <h5 className="txt-light">Student at CESI engineering school</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
-export default header
+export default Header
